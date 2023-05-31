ec37dcdf4d39ef3530d42c2a97382025523ba045
iamsanchezmartin <127458743+iamsanchezmartin@users.noreply.github.com>
2023-05-31 10:28:51 -0300

    Actualización 30/05/23

---
+++ myscript.js
@@ -0,0 +1,10 @@
+let numero1= Math.random()
+document.getElementById("capchatnumero1").innerHTML=numero1
+
+let numero2= Math.random()
+document.getElementById("capchatnumero2").innerHTML=numero2
+
+
+const resultado= numero1 + numero2
+
+let respuesta= null
