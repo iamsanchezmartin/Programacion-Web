a906b78d64b710bf48045372e9cbf9780ebb9f74
iamsanchezmartin <127458743+iamsanchezmartin@users.noreply.github.com>
2023-06-06 21:22:32 -0300

    Actualización 06/06/2023

--- myscript.js
+++ myscript.js
@@ -1,10 +1,16 @@
-let numero1= Math.random()
+let numero1= Math.floor( Math.random()*10)
 document.getElementById("capchatnumero1").innerHTML=numero1
 
-let numero2= Math.random()
+let numero2= Math.floor( Math.random()*10)
 document.getElementById("capchatnumero2").innerHTML=numero2
 
 
-const resultado= numero1 + numero2
+const resultado = numero1 + numero2
 
-let respuesta= null
+function botondesendmenssaje() {
+    let respuesta = document.getElementById("respuestadelformulario");
+    if respuesta == resultado
+        
+}
+
+
